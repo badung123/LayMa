0597c079028f4ac26cd41737c030e7b6791c2c2c
Kidman <dung7166@gmail.com>
2024-10-09 22:26:39 +0700

    identity

---
+++ src/LayMa.Core/Domain/Identity/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LayMa.Core.Domain.Identity
+{
+    [Table("AppUsers")]
+    public class AppUser : IdentityUser<Guid>
+    {
+        public bool IsActive { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public double Balance { get; set; }
+        public string? UserTelegram { get; set; }
+        public string? RefCode { get; set; }
+    }
+}
--- src/LayMa.WebAPI/Program.cs
+++ src/LayMa.WebAPI/Program.cs
@@ -1,4 +1,39 @@
+using LayMa.Core.Domain.Identity;
+using LayMa.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+//Config DB Context and ASP.NET Core Identity
+builder.Services.AddDbContext<LayMaContext>(options =>
+                options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<LayMaContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
 
 // Add services to the container.
 
