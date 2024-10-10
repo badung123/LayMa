05966493a919d7f927e8b2a9ec78ef522735c19b
Kidman <dung7166@gmail.com>
2024-10-10 12:02:18 +0700

    add repository and unitofwork base

---
+++ src/LayMa.Data/SeedWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using LayMa.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayMa.Data.SeedWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LayMaContext _context;
+
+        public UnitOfWork(LayMaContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
