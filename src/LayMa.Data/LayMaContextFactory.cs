1274a8435ca2b3f2becf70da63931184cd95ab0e
Kidman <dung7166@gmail.com>
2024-10-10 09:56:52 +0700

    initial migration

---
+++ src/LayMa.Data/LayMaContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayMa.Data
+{
+    public class LayMaContextFactory : IDesignTimeDbContextFactory<LayMaContext>
+    {
+        public LayMaContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<LayMaContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new LayMaContext(builder.Options);
+        }
+    }
+}
